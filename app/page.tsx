74c60056101b1f9578981809c56e7da3bfc7bbcf
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2026-02-23 20:02:09 -0300

    commit inicial

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div>
+      <ol>
+        <li>João Guilherme</li>
+        <li>José Lorenzo</li>
+        <li>Edson Hagmayer</li>
+        <li>Fabrizio Astrada</li>
+        <li>Pedro Ferreira</li>
+        <li>Kleberson dos Santos</li>
+        <li>João Vítor</li>
+        <li>Leandro Bugalho</li>
+        <li>Clarissa Flor</li>
+        <li>Amanda dos Santos Daros</li>
+        <li>Jhon Vítor Medeiros</li>
+        <li>Elias Caleb</li>
+        <li>Diogo Maia</li>
+        <li>Eduardo Ribeiro</li>
+        <li>Tiago Bugalho</li>
+        <li>Isabelly Marques</li>
+        <li>Mateus Collet Lacerda Machado</li>
+      </ol>
+    </div>
+  );
+}

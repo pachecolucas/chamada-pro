f141e814a4e2fbd150f3a9a9d2037585a9febc6c
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2026-03-09 20:14:13 -0300

    Update route.ts

--- app/api/estudantes/route.ts
+++ app/api/estudantes/route.ts
@@ -2,5 +2,5 @@ import { E } from "@/app/model";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(E);
+  return NextResponse.json(Object.values(E).sort((a, b) => a.nome.localeCompare(b.nome)));
 }

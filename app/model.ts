f2b6c7ddcc9e4049fa50c0f6cf54fe35787a3dc1
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2026-03-09 19:28:52 -0300

    avanços

--- app/model.ts
+++ app/model.ts
@@ -1,25 +1,103 @@
 type Dia = {
   data: string;
-  estudantes: EstudanteId[];
+  estudantes: Estudante[];
 };
 
 type Estudante = {
-  id: number;
   nome: string;
 };
 
-export const ESTUDANTES: Record<string, Estudante> = {
-  joao_guilherme: { id: 1, nome: "João Guilherme" },
-  joao_pedro: { id: 2, nome: "João Pedro" },
-  maria_eduarda: { id: 3, nome: "Maria Eduarda" },
-  ricardo_pereira: { id: 4, nome: "Ricardo Pereira" },
+export const E: Record<string, Estudante> = {
+  joao_guilherme: { nome: "João Guilherme" },
+  jose_lorenzo: { nome: "José Lorenzo" },
+  edson_hagmayer: { nome: "Edson Hagmayer" },
+  fabrizio_astrada: { nome: "Fabrizio Astrada" },
+  pedro_ferreira_nunes_neto: { nome: "Pedro Ferreira Nunes Neto" },
+  kleberson_dos_santos: { nome: "Kleberson dos Santos" },
+  joao_vitor_marcolino_veiga: { nome: "João Vítor Marcolino Veiga" },
+  leandro_bugalho: { nome: "Leandro Bugalho" },
+  clarissa_flor: { nome: "Clarissa Flor" },
+  amanda_dos_santos_daros: { nome: "Amanda dos Santos Daros" },
+  jhon_vitor_medeiros: { nome: "Jhon Vítor Medeiros" },
+  elias_caleb: { nome: "Elias Caleb" },
+  diogo_maia: { nome: "Diogo Maia" },
+  eduardo_ribeiro: { nome: "Eduardo Ribeiro" },
+  tiago_bugalho: { nome: "Tiago Bugalho" },
+  isabelly_marques: { nome: "Isabelly Marques" },
+  mateus_collet_lacerda_machado: { nome: "Mateus Collet Lacerda Machado" },
+  joao_pedro_mello: { nome: "João Pedro Mello" },
+  igor_da_silva_dias: { nome: "Igor da Silva Dias" },
+  sabrina_da_silva_sobral: { nome: "Sabrina da Silva Sobral" },
+  railson_de_oliveira_silva: { nome: "Railson de Oliveira Silva" },
+  rafa_verde: { nome: "Rafa Verde" },
 };
 
-type EstudanteId = keyof typeof ESTUDANTES;
-
 export const DIAS: Dia[] = [
-  { data: "2026-03-02", estudantes: ["joao_guilherme", "maria_eduarda"] },
-  { data: "2026-03-09", estudantes: ["maria_eduarda", "ricardo_pereira"] },
+  {
+    data: "2026-02-23",
+    estudantes: [
+      E.joao_guilherme,
+      E.jose_lorenzo,
+      E.edson_hagmayer,
+      E.fabrizio_astrada,
+      E.pedro_ferreira_nunes_neto,
+      E.kleberson_dos_santos,
+      E.joao_vitor_marcolino_veiga,
+      E.leandro_bugalho,
+      E.clarissa_flor,
+      E.amanda_dos_santos_daros,
+      E.jhon_vitor_medeiros,
+      E.elias_caleb,
+      E.diogo_maia,
+      E.eduardo_ribeiro,
+      E.tiago_bugalho,
+      E.isabelly_marques,
+      E.mateus_collet_lacerda_machado,
+    ],
+  },
+  {
+    data: "2026-03-02",
+    estudantes: [
+      E.pedro_ferreira_nunes_neto,
+      E.joao_pedro_mello,
+      E.igor_da_silva_dias,
+      E.joao_vitor_marcolino_veiga,
+      E.sabrina_da_silva_sobral,
+      E.amanda_dos_santos_daros,
+      E.railson_de_oliveira_silva,
+      E.edson_hagmayer,
+      E.isabelly_marques,
+      E.joao_guilherme,
+      E.mateus_collet_lacerda_machado,
+      E.leandro_bugalho,
+      E.tiago_bugalho,
+      E.kleberson_dos_santos,
+      E.elias_caleb,
+      E.jose_lorenzo,
+      E.rafa_verde,
+      E.fabrizio_astrada,
+    ],
+  },
+  {
+    data: "2026-03-09",
+    estudantes: [
+      E.railson_de_oliveira_silva,
+      E.edson_hagmayer,
+      E.mateus_collet_lacerda_machado,
+      E.amanda_dos_santos_daros,
+      E.sabrina_da_silva_sobral,
+      E.joao_pedro_mello,
+      E.elias_caleb,
+      E.kleberson_dos_santos,
+      E.jhon_vitor_medeiros,
+      E.igor_da_silva_dias,
+      E.joao_vitor_marcolino_veiga,
+      E.rafa_verde,
+      E.tiago_bugalho,
+      E.clarissa_flor,
+      E.fabrizio_astrada,
+    ],
+  },
 ];
 
-console.log(ESTUDANTES, DIAS);
+console.log(DIAS);
--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,39 @@
 import Image from "next/image";
 import foto from "./pro-260223.jpeg";
-import { DIAS, ESTUDANTES } from "./model";
+import { DIAS, E } from "./model";
 
 export default function Home() {
   return (
     <div>
-      <h1>Estudantes</h1>
-      {/* {ESTUDANTES.map((e) => (
-        <div key={e.id}>
-          {e.id} {e.nome}
-        </div>
-      ))} */}
-
-      <h1>Chamadas</h1>
-      <div className="flex gap-4">
-        {DIAS.map((d) => (
-          <div key={d.data} className="border p-4">
-            <h2>{d.data}</h2>
-            {d.estudantes.map((key) => (
-              <div key={key}>{ESTUDANTES[key].nome}</div>
+      <h1>Chamadas ELITI PRO 2026</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            {DIAS.map((d) => (
+              <th key={d.data} className="p-1 text-center">
+                {d.data}
+              </th>
             ))}
-          </div>
-        ))}
-      </div>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(E)
+            .sort((a, b) => a.nome.localeCompare(b.nome))
+            .map((e, i) => (
+              <tr key={i}>
+                <td className="p-1">{e.nome}</td>
+                {DIAS.map((d) => (
+                  <td key={d.data} className="p-1 text-center">
+                    {d.estudantes.includes(e) ? "✅" : "❌"}
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
 
       <Image src={foto.src} width={foto.width} height={foto.height} alt="foto" className="w-100" />
-      <h1>23/02/26</h1>
-      <ol>
-        <li>João Guilherme</li>
-        <li>José Lorenzo</li>
-        <li>Edson Hagmayer</li>
-        <li>Fabrizio Astrada</li>
-        <li>Pedro Ferreira Nunes Neto</li>
-        <li>Kleberson dos Santos</li>
-        <li>João Vítor Marcolino Veiga</li>
-        <li>Leandro Bugalho</li>
-        <li>Clarissa Flor</li>
-        <li>Amanda dos Santos Daros</li>
-        <li>Jhon Vítor Medeiros</li>
-        <li>Elias Caleb</li>
-        <li>Diogo Maia</li>
-        <li>Eduardo Ribeiro</li>
-        <li>Tiago Bugalho</li>
-        <li>Isabelly Marques</li>
-        <li>Mateus Collet Lacerda Machado</li>
-      </ol>
-      <h1>02/03/26</h1>
-      <ol>
-        <li>Pedro Ferreira Nunes Neto</li>
-        <li>João Pedro Mello</li>
-        <li>Igor da Silva Dias</li>
-        <li>João Vítor Marcolino Veiga</li>
-        <li>Sabrina da Silva Sobral</li>
-        <li>Amanda dos Santos Daros</li>
-        <li>Railson de Oliveira Silva</li>
-        <li>Edson Hagmayer</li>
-        <li>Isabelly Marques</li>
-        <li>João Guilherme</li>
-        <li>Mateus Collet Lacerda Machado</li>
-        <li>Leandro Bugalho</li>
-        <li>Tiago Bugalho</li>
-        <li>Kleberson dos Santos</li>
-        <li>Elias Caleb</li>
-        <li>José Lorenzo</li>
-        <li>Rafa Verde</li>
-      </ol>
     </div>
   );
 }
